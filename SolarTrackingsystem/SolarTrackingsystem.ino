8ffe401199640ae9b9fd5564ad198fd30b89c5df
harshchotaliya7 <20bec039@nirmauni.ac.in>
2023-10-26 19:17:33 +0530

    elevation and azimuth angle is getting correct only motor calibration remaining

--- SolarTrackingsystem/SolarTrackingsystem.ino
+++ SolarTrackingsystem/SolarTrackingsystem.ino
@@ -3,6 +3,7 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include <LCD_I2C.h>
+#include <math.h>
 
 /************* Disclaimer ********************* 
 *  Minor Project 2023 
@@ -13,9 +14,16 @@
 *  there then RTC is used for timining.
 */
 
+#define default_long  72.54
+#define default_lat   23.13
+
+///////////////////// degree to radian
+#define DEG_TO_RAD   0.01744   
+#define RAD_TO_DEG   57.32484 
+
 ///////////////////// Timezone offset for gps timing.
-#define  timeZoneOffsetHours    6  
-#define  timeZoneOffsetMinutes  -30
+#define  timeZoneOffsetHours    -6  
+#define  timeZoneOffsetMinutes  30
 
 ///////////////////// Row and column of lcd
 #define row_lcd   4
@@ -35,22 +43,27 @@ LCD_I2C lcd(0xA7,col_lcd,row_lcd);
 uRTCLib rtc(0x68);
 
 int prev_hour=0,prev_minute=0,prev_second=0;
+double latitude_=0,longitude_=0, ele_ang, azi_ang;
+long JDate;
 
 //////////////////// Weak Days array
-char daysOfTheWeek[7][12] = {"Sun", "Monday", "Tuesday", "Wednesday", "Thursday", "Fri", "Saturday"};
+char daysOfTheWeek[7][12] = {"Sun", "Monday", "Tuesday", "Wed", "Thursday", "Fri", "Saturday"};
 
 ////////////////// GPS Module (runs on Serial1)
 const uint32_t GPSBaud = 9600; 
 
-int localMinutes,localHours;
+int UTCMinutes,UTCHours,dayofyear;
 ////////////////// UTC to local time converter function 
-int UTCtoLocalTime(int hour, int minute);
+int LocaltoUTCTime(int hour, int minute);
 
 /////////////////// LCD/Serial Printing Function
 void Print_data(bool L_S,bool gpsorrtc);
 
 /////////////////// Sun position Function
-void Calculate_Sun_Position(int hour , int minute , int second , int day , int month , int year);
+void Calculate_Sun_Position(int hour , int minute , int second , int day , int month , int year, float longitude , float latitude);
+
+////////////////// day of the year
+void day_of_the_year(int day, int month, int year);
 
 ////////////////// Julian Date Function
 long JulianDate(int year, int month, int day);
@@ -73,10 +86,11 @@ void setup() {
   URTCLIB_WIRE.begin();
   
   ///////////////// First time manually set the RTC data when POWER ON
-  //rtc.set(0,20,11,1,22,10,23);
+  //rtc.set(50,23,18,5,26,10,23);
 }
 
 void loop() {
+
   if (Serial1.available() > 0) 
   {
     ///// print only if gps data is encoded correctly
@@ -86,58 +100,197 @@ void loop() {
       ///// also only print correct data if location is valid 
       if (gps.location.isValid()) 
       {
-        Print_data(LCD_,GPS_);
+        latitude_=gps.location.lat();
+        longitude_=gps.location.lng();
       } 
-
       ///// other wise print invalid 
       else 
       {
+        latitude_= default_lat;
+        longitude_ = default_long;
         lcd.print(F("- location: INVALID"));
         Serial.print("location invalid");
       }
+      
     }
+       
    }
-      ////// RTC to refresh every time 
-      rtc.refresh();
- 
-      /******************************************************
-      ** To view RTC Data on Serial Monitor / LCD ***********
-      ******************************************************/ 
-      Print_data(LCD_,RTC_);
+   
 
-      prev_hour=rtc.hour();
-      prev_minute=rtc.minute();
-      prev_second=rtc.second();
+  rtc.refresh();
 
+  /*********************************************************************************
+  ***** Local Time to UTC Time Convert for Sun position calulation *****************
+  *********************************************************************************/
+  
+  LocaltoUTCTime(rtc.hour(),rtc.minute());
+  
+  /******************************************************
+  ************* Sun position Calculation ****************
+  ******************************************************/
+  
+  Calculate_Sun_Position(UTCHours,UTCMinutes,rtc.second(),rtc.day(),rtc.month(),rtc.year(),default_long,default_lat);
+   
+  /******************************************************
+  ************* To view Elevation Data on LCD ***********
+  ******************************************************/ 
+
+  lcd.setCursor(0,0);
+  lcd.print("elevation:");
+  lcd.setCursor(12, 0);
+  lcd.print(ele_ang);
+  lcd.print("\337");
+
+  /******************************************************
+  ************* To view Azimuth Data on LCD *************
+  ******************************************************/
+
+  lcd.setCursor(0,1);
+  lcd.print("azimuth:");
+  lcd.setCursor(12,1);
+  lcd.print(azi_ang);
+  lcd.print("\337");
+
+  /******************************************************
+  ************* To view UTC Time on LCD *****************
+  ******************************************************/
+
+  lcd.setCursor(0,2);
+  lcd.print("UTC Time:");
+  lcd.setCursor(12,2);
+  if(UTCHours<10)
+  {
+    lcd.print("0");
+  }
+  lcd.print(UTCHours);
+  lcd.print(":");
+  if(UTCMinutes<10)
+  {
+    lcd.print("0");
+  }
+  lcd.print(UTCMinutes);
+  lcd.print(":");
+  if(rtc.second()<10)
+  {
+    lcd.print("0");
+  }
+  lcd.print(rtc.second());
+
+  /******************************************************
+  *********** To view Local Time on LCD *****************
+  ******************************************************/
+
+  lcd.setCursor(0,3);
+  lcd.print("Local Time:");
+  lcd.setCursor(12, 3);
+  if(rtc.hour()<10)
+  {
+    lcd.print("0");
+  }
+  lcd.print(rtc.hour());
+  lcd.setCursor(14,3);
+  lcd.print(':');
+  if(rtc.minute()<10)
+  {
+    lcd.print("0");
+  }
+  lcd.print(rtc.minute());
+  lcd.setCursor(17,3);
+  lcd.print(':');
+  if(rtc.second()<10)
+  {
+    lcd.print("0");
+  }
+  lcd.print(rtc.second());
+  lcd.setCursor(18,3);
+  
+  /******************************************************
+  ****** To change on LCD data on every second **********
+  ******************************************************/
+
+  if( (rtc.hour() != prev_hour) | (rtc.minute() != prev_minute) | (rtc.second() != prev_second) )
+  {
+    lcd.clear();
+  }
+
+  prev_hour=rtc.hour();
+  prev_minute=rtc.minute();
+  prev_second=rtc.second();
 }
 
-int UTCtoLocalTime(int hour, int minute)
+int LocaltoUTCTime(int hour, int minute)
 {
   // Calculate the local time
-  localHours = hour + timeZoneOffsetHours;
-  localMinutes = minute + timeZoneOffsetMinutes;
+  UTCHours = hour + timeZoneOffsetHours;
+  UTCMinutes = minute + timeZoneOffsetMinutes;
 
   // Adjust for negative minutes or overflow
-  if (localMinutes < 0) {
-    localMinutes += 60;
-    localHours -= 1;
+  if (UTCMinutes < 0) {
+    UTCMinutes += 60;
+    UTCHours -= 1;
   }
-  if (localMinutes >= 60) {
-    localMinutes -= 60;
-    localHours += 1;
+  if (UTCMinutes >= 60) {
+    UTCMinutes -= 60;
+    UTCHours += 1;
   }
 }
 
-void Calculate_Sun_Position(int hour , int minute , int second , int day , int month , int year)
+void Calculate_Sun_Position(int hour, int minute, int second, int day, int month, int year,float longitude , float latitude) 
 {
   float T, JD_frac, L0, M, e, C, L_true, f, R, GrHrAngle, Obl, RA, Decl, HrAngle;
   long JDate, JDx;
+  
+  ////////// Julian Date
+  JDate      = JulianDate(year, month, day);
 
-  JDate = JulianDate(year,month,day);
+  ////////// Julian Time 
+  JD_frac    = (hour + minute / 60.0 + second / 3600.0) / 24.0 - 0.5;
+
+  ////////// Substract Time reference 1 January 2000
+  T          = JDate - 2451545; 
+  T          = (T + JD_frac) / 36525.0;
+
+  ////////// Mean Logitude
+  L0         = DEG_TO_RAD * fmod(280.46645 + 36000.76983 * T, 360);
+
+  ////////// Mean Anomaly
+  M          = DEG_TO_RAD * fmod(357.5291 + 35999.0503 * T, 360);
+  e          = 0.016708617 - 0.000042037 * T;
+  C          = DEG_TO_RAD * ((1.9146 - 0.004847 * T) * sin(M) + (0.019993 - 0.000101 * T) * sin(2 * M) + 0.00029 * sin(3 * M));
+  f          = M + C;
+
+  ///////// obliquity of the ecliptic
+  Obl        = DEG_TO_RAD * (23 + 26 / 60.0 + 21.448 / 3600. - 46.815 / 3600 * T);
+  JDx        = JDate - 2451545;
+  GrHrAngle  = 280.46061837 + (360 * JDx) % 360 + 0.98564736629 * JDx + 360.98564736629 * JD_frac;
+  GrHrAngle  = fmod(GrHrAngle, 360.0);
+
+  ///////// ecliptic logitude
+  L_true     = fmod(C + L0, 2 * PI);
+
+  R          = 1.000001018 * (1 - e * e) / (1 + e * cos(f));
+  ///////// Right ascension
+  RA         = atan2(sin(L_true) * cos(Obl), cos(L_true));
+
+  ///////// declination
+  Decl       = asin(sin(Obl) * sin(L_true));
+
+  ///////// Hour angle
+  HrAngle    = (DEG_TO_RAD * GrHrAngle) + (longitude * DEG_TO_RAD) - RA;
+  
+  // Azimuth measured east from north, so 0 degrees is North
+
+  ///////// Elevation Angle
+  ele_ang  = asin(sin(latitude*DEG_TO_RAD) * sin(Decl) + cos(latitude*DEG_TO_RAD) * (cos(Decl) * cos(HrAngle)));
+  ele_ang = ele_ang / DEG_TO_RAD;
+
+  ///////// Azimuth angle
+  azi_ang  = PI + atan2(sin(HrAngle), cos(HrAngle) * sin(latitude*DEG_TO_RAD) - tan(Decl) * cos(latitude*DEG_TO_RAD)); 
+  azi_ang   = azi_ang   / DEG_TO_RAD + 0.6;
+  
 }
 
-long JulianDate(int year, int month, int day)
-{
+long JulianDate(int year, int month, int day) {
   long JDate;
   int A, B;
   if (month <= 2) {year--; month += 12;}
@@ -146,12 +299,27 @@ long JulianDate(int year, int month, int day)
   return JDate;
 }
 
+void day_of_the_year(int day, int month, int year)
+{
+  int day_in_month[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+  dayofyear=day;
+  if(year%4==0)
+  {
+    day_in_month[2]=29;
+  }
+  for(int i=0;i<month;i++)
+  {
+    dayofyear+=day_in_month[i];
+  }
+}
+
+
 void Print_data(bool L_S,bool gpsorrtc)
 {
     if(L_S == LCD_)
     {
-      if(gpsorrtc==GPS_)
-      { 
+       if(gpsorrtc == GPS_)
+       { 
         lcd.setCursor(0, 0);
         lcd.print("Latitude:") ;
         lcd.setCursor(10, 0);
@@ -160,9 +328,8 @@ void Print_data(bool L_S,bool gpsorrtc)
         lcd.print("Longitude:") ;
         lcd.setCursor(11, 1);
         lcd.print(gps.location.lng());
-      }
-      else
-      {
+       }
+   
         lcd.setCursor(0, 2);
         lcd.print("Date:");
         lcd.print(rtc.day());
@@ -173,7 +340,7 @@ void Print_data(bool L_S,bool gpsorrtc)
         lcd.print(" (");
         lcd.print(daysOfTheWeek[rtc.dayOfWeek()-1]);
         lcd.print(") ");
-
+  
         lcd.setCursor(0,3);
         lcd.print("Time:");
         lcd.setCursor(5, 3);
@@ -193,42 +360,41 @@ void Print_data(bool L_S,bool gpsorrtc)
         {
           lcd.clear();
         }
-      }
-  }
-  else
-  {
-    if(gpsorrtc==GPS_)
-    {
-      Serial.print(" latitude : ");
-      Serial.print(gps.location.lat());
-      Serial.print(" longitude : ");
-      Serial.println(gps.location.lng());
-    } 
+    }
     else
     {
-      Serial.print("Date :");
-      Serial.print(rtc.day());
-      Serial.print('/');
-      Serial.print(rtc.month());
-      Serial.print('/');
-      Serial.print(rtc.year());
-      Serial.print(" (");
-      Serial.print(daysOfTheWeek[rtc.dayOfWeek()-1]);
-      Serial.println(") ");
+      if(gpsorrtc==GPS_)
+      {
+        Serial.print(" latitude : ");
+        Serial.print(gps.location.lat());
+        Serial.print(" longitude : ");
+        Serial.println(gps.location.lng());
+      } 
+      else
+      {
+        Serial.print("Date :");
+        Serial.print(rtc.day());
+        Serial.print('/');
+        Serial.print(rtc.month());
+        Serial.print('/');
+        Serial.print(rtc.year());
+        Serial.print(" (");
+        Serial.print(daysOfTheWeek[rtc.dayOfWeek()-1]);
+        Serial.println(") ");
 
-      Serial.print("Time :");
-      Serial.print(rtc.hour());
-      Serial.print(':');
-      Serial.print(rtc.minute());
-      Serial.print(':');
-      Serial.println(rtc.second());
+        Serial.print("Time :");
+        Serial.print(rtc.hour());
+        Serial.print(':');
+        Serial.print(rtc.minute());
+        Serial.print(':');
+        Serial.println(rtc.second());
 
-      Serial.print("Temperature: ");
-      Serial.print(rtc.temp()  / 100);
-      Serial.print("\xC2\xB0");  
-      Serial.println("C");
-      Serial.println("--------------------------------");
-      delay(900);
+        Serial.print("Temperature: ");
+        Serial.print(rtc.temp()  / 100);
+        Serial.print("\xC2\xB0");  
+        Serial.println("C");
+        Serial.println("--------------------------------");
+        delay(900);
+      }
     }
-  }
 }
